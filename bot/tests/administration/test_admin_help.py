b7dba22426ce2a1c926c5939a8811590d153255d
skelly37 <skelly37@protonmail.com>
2026-01-06 18:33:25 +0100

    test requests

--- bot/tests/administration/test_admin_help.py
+++ bot/tests/administration/test_admin_help.py
@@ -8,11 +8,47 @@ from bot.tests.base_test import BaseTest
 class TestAdminHelpHandler(BaseTest):
     @pytest.mark.asyncio
     async def test_admin_help(self):
+        import requests
+        BASE_URL = "http://192.168.1.210:8199/api/v1"
+
+        login_payload = {
+            "username": "Admin",
+            "password": "qykjeq-9sybqo-joJfyq"
+        }
+
+        with requests.Session() as session:
+            response = session.post(f"{BASE_URL}/auth/login", json=login_payload)
+            response.raise_for_status()
+
+            token = response.json()["access_token"]
+
+            # Authorized Request
+            headers = {
+                "Authorization": f"Bearer {token}"
+            }
+            start_payload = {
+                "args": ["lista"]
+            }
+
+            response_start = session.post(
+                f"{BASE_URL}/start",
+                json=start_payload,
+                headers=headers
+            )
+            response_start.raise_for_status()
+
+            print(response_start.status_code)
+            print(response_start.text)
+
+            assert response_start.status_code == 200
+            assert response_start.text is not None
+
         await self.expect_command_result_contains(
             'admin',
             [await self.remove_first_line(await self.get_response(RK.ADMIN_HELP))],
         )
 
+    @pytest.mark.skip(reason="ashdahsd")
     @pytest.mark.asyncio
     async def test_admin_shortcuts(self):
         await self.expect_command_result_contains(
@@ -21,6 +57,7 @@ class TestAdminHelpHandler(BaseTest):
             args=['skroty']
         )
 
+    @pytest.mark.skip(reason="ashdahsd")
     @pytest.mark.asyncio
     async def test_admin_invalid_command(self):
         await self.expect_command_result_contains(
