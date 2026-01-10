0e244963b02899c3176fbf8193133cee972342b6
skelly37 <skelly37@protonmail.com>
2026-01-10 01:05:17 +0100

    Deleted Season 11 easter egg

--- bot/handlers/not_sending_videos/episode_list_handler.py
+++ bot/handlers/not_sending_videos/episode_list_handler.py
@@ -50,15 +50,6 @@ class EpisodeListHandler(BotMessageHandler):
         season_info = await TranscriptionFinder.get_season_details_from_elastic(logger=self._logger)
         episodes = await TranscriptionFinder.find_episodes_by_season(season, self._logger)
 
-        context = {
-            "season": season,
-            "specialized_table": s.SPECIALIZED_TABLE,
-            "episodes": episodes,
-        }
-
-        if await self.__check_easter_eggs(context):
-            return None
-
         if not episodes:
             return await self.__reply_no_episodes_found(season)
 
@@ -84,24 +75,6 @@ class EpisodeListHandler(BotMessageHandler):
     async def __reply_no_episodes_found(self, season: int) -> None:
         await self.reply_error(RK.NO_EPISODES_FOUND, args=[str(season)])
         await self._log_system_message(logging.INFO, get_log_no_episodes_found_message(season))
-
-    async def __handle_ranczo_season_11(self) -> None:
-        image_path = Path("Ranczo_Sezon11.png")
-        with image_path.open("rb") as f:
-            image_bytes = f.read()
-        await self._answer_photo(image_bytes, image_path, caption=get_season_11_petition_message())
-
-    @staticmethod
-    def __is_ranczo_season_11(context: dict) -> bool:
-        return (
-            context["season"] == 11
-            and context["specialized_table"] == "ranczo_messages"
-        )
-
-    def __get_easter_eggs(self) -> List[Tuple[isSeasonCustomFn, onCustomSeasonFn]]:
-        return [
-            (self.__is_ranczo_season_11, self.__handle_ranczo_season_11),
-        ]
 
     async def __check_easter_eggs(self, context: dict) -> bool:
         for predicate, callback in self.__get_easter_eggs():
