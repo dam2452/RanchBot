a2a5b5f9b75ee5bf0f10d60b1ae334563bb6acb0
skelly37 <skelly37@protonmail.com>
2026-01-24 16:23:51 +0100

    test inline mode

--- bot/factory/permission_level_factory.py
+++ bot/factory/permission_level_factory.py
@@ -17,7 +17,10 @@ from aiogram import (
     Dispatcher,
 )
 from aiogram.filters import Command
-from aiogram.types import Message as AiogramMessage
+from aiogram.types import (
+    InlineQuery,
+    Message as AiogramMessage,
+)
 
 from bot.adapters.telegram.telegram_message import TelegramMessage
 from bot.adapters.telegram.telegram_responder import TelegramResponder
@@ -45,6 +48,11 @@ class PermissionLevelFactory(ABC):
             dp.message.middleware.register(AiogramMiddlewareAdapter(middleware))
 
         self._logger.info(f"{self.__class__.__name__} middlewares registered")
+
+        inline_handler = self.get_inline_handler()
+        if inline_handler:
+            dp.inline_query.register(inline_handler)
+            self._logger.info(f"{self.__class__.__name__} inline handler registered")
 
     def get_telegram_routes_and_middlewares(self) -> Tuple[
         List[Tuple[str, Callable[[AiogramMessage], Awaitable[None]]]],
@@ -90,3 +98,33 @@ class PermissionLevelFactory(ABC):
     @abstractmethod
     def create_middlewares(self, commands: List[str]) -> List[BotMiddleware]:
         pass
+
+    def get_inline_handler(self) -> Optional[Callable[[InlineQuery], Awaitable[None]]]:
+        handlers_with_inline = [
+            handler_cls
+            for handler_cls in self.create_handler_classes()
+            if handler_cls(message=None, responder=None, logger=self._logger).supports_inline_mode()
+        ]
+
+        if not handlers_with_inline:
+            return None
+
+        async def inline_handler(inline_query: InlineQuery):
+            query = inline_query.query.strip()
+            all_results = []
+
+            for handler_cls in handlers_with_inline:
+                handler = handler_cls(message=None, responder=None, logger=self._logger)
+                results = await handler.handle_inline_query(query)
+                if results:
+                    all_results.extend(results)
+
+            await inline_query.answer(
+                results=all_results,
+                cache_time=1,
+                is_personal=True,
+            )
+
+            self._logger.info(f"Inline query handled for user {inline_query.from_user.id}: '{query}'")
+
+        return inline_handler
